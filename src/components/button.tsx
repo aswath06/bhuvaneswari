fbb9c079874c2384df33b86c7ee84dd534bf1b76
aswath06 <maswath55@gmail.com>
2024-12-19 20:45:03 +0530

    components entery

---
+++ src/components/button.tsx
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function button() {
+  return (
+    <View>
+      <Text>button</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
